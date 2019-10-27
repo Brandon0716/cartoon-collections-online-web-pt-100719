6e8f86054cf2c234fb57daf53110cf8f59bf2926
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-27 19:27:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,9 @@ def long_planeteer_calls(joke)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(joker)
+ cheese_types = ["cheddar", "gouda", "camembert"]
+    joker.find do |type|
+    cheese_types.include?(type)
+  end 
 end
